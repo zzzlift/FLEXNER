aa33832f43a514d5f727d403754e284a0d4ccbbb
zzzlift <zhyxhys@163.com>
2019-11-05 10:14:01 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## FlexNER ([Project Tutorial](https://liftkkkk.github.io/FLEXNER/))
 
-#### As long as this paper is accepted, this toolkit can be downloaded.
++++ updating +++
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-brightgreen.svg)](https://opensource.org/licenses/Apache-2.0)
 
@@ -22,6 +22,7 @@ FlexNER is a toolkit of neural NER models designed to accelerate ML research. Th
   * [NYT](#nyt)
   * [IOB,IOB2,BIOES Conversion](#iob,iob2,bioes-conversion)
   * [Using Your Data](#using-your-data)
+* [Story of FlexNER](#story-of-flexner)
 * [Updating](#updating)
 
     
@@ -59,17 +60,17 @@ optional arguments:
 
   For the Baseline model
 ```
-python train.py -a base 
+bash run_n1.sh
 ```
 For the Joint training
 ```
-python train.py -a join
+python run_n1.sh with -g1=0 -r1=0 -g2=0 -r2=0
 ```
 For the separated training
 ```
-(1) python train.py -a join -r2 1 [-g2 1]
-(2) python train.py -a join -r1 1 [-g1 1] -mp model_path -m tune
-(3) python train.py -a join -g1 1 -g2 1 -mp model_path -m tune
+(1) bash run_n1.sh
+(2) bash run_n2.sh
+(3) bash run_n1.sh with -g1=1 -r1=0 -g2=1 -r2=0
 ```
 
 ### Addition
@@ -80,11 +81,11 @@ class Bi_Stacka(Bi_NER):
     # initialize the constructor
     ...
     
-    # defined a arch.
+    # define a arch.
     def mix(self):
         # 1. add the embeddings
         self.base_embed=self.embedding_layer_base()
-		
+    
         # 2. define your arch.
         encode1=self.mix_stacka('net1')
         encode2=self.mix_stacka('net2')
@@ -94,10 +95,10 @@ class Bi_Stacka(Bi_NER):
          self.encode=tf.concat([encode1,encode2,encode3],axis=-1)
 
         # additional process
-    	...
-    	
-    	# 3. add a crf layer
-    	self.crf_layer()
+      ...
+      
+      # 3. add a crf layer
+      self.crf_layer()
 ```
 #### Language Correlation
 This framework can also be applied to multilingual research.   
@@ -113,13 +114,22 @@ These sub-networks can also be combined to asynchronously train different langua
 
 ## Suggested Datasets
 
-+ CoNLL-2003 dataset [link](https://github.com/synalp/NER/tree/master/corpus/CoNLL-2003)  
++ CoNLL-2003 English [link](https://github.com/synalp/NER/tree/master/corpus/CoNLL-2003)  
++ CoNLL-2003 German [link](https://www.clips.uantwerpen.be/conll2003/ner/) 
++ CoNLL-2002 Spanish [link](https://github.com/teropa/nlp/tree/master/resources/corpora/conll2002)
++ CoNLL-2002 Dutch [link](https://github.com/teropa/nlp/tree/master/resources/corpora/conll2002)
++ Chinese [link](https://github.com/lancopku/Chinese-Literature-NER-RE-Dataset)
 + NYT dataset [link](https://github.com/shanzhenren/CoType)  
++ Biomedical domain [link]()  
+The augmented data set can be 3-10 times the original training data.
 
+## Story of FlexNER
+Some friends may want to know why you are doing this, and now we have stronger models. Because this framework has a story. At the very begining (2017-Sep), the idea of entity augmentation is a sort of simple because I only use shallow network blocks (i.e. LSTM layer and additive attention), so it naturally does not meet the requirements for publishing the paper. Besides, paper writing needs to be improved. I think I should introduce some more complicated mechanisms, so I tried several attentions, but the improvement is not obvious. I think there is a kind of <strong>context common sense</strong>, which is why I proposed this method. <strong>Context common sense</strong> is different from the KB common sense. This theory is not perfect enough, so it is naturally challenged in another proposal. I am just talking about it to generate more inspiration. This may include immature ideas, so this theory needs to be explored more before it is completed. Then, 2018-Nov, BERT completely beats this approach. I can not think of using the effective transformer blocks. (:(|)!!!  
+
+This story may answer the questions.
 
 ## Updating...
-
-* 2017-Sep-10, Bi_NER v0.1, initial version
-* 2018-Apr-05, Bi_NER v0.2, supporting easily customizing architecture and attention mechanism
-* 2018-Nov-03, FlexNER v0.3, supporting different languages ( tested on English, German, Spanish, Dutch) and biomedical domain
 * 2019-Mar-20, FlexNER v0.3, reconstructing the code
+* 2018-Nov-03, FlexNER v0.3, support different languages ( tested on English, German, Spanish, Dutch) and biomedical domain
+* 2018-Apr-05, Bi_NER v0.2, support easily customizing architecture and more attention mechanism
+* 2017-Sep-10, Bi_NER v0.1, initial version, present NER data augmentation 
--- model/Joint.py
+++ model/Joint.py
@@ -1,114 +1,51 @@
-
+#author: Hongyin Zhu
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("..")
 import tensorflow as tf
 import numpy as np
-from util import *
-from config import *
-from lstm_crf_v2 import *
+from utils.util import *
+from model.LSTMCRF import *
 
-class ATT_J(LSTMCRF):
-	def __init__(self, train='train',stop1=0,stop2=0,stop3=0,stop4=0,mask1=0,mask2=0,mask3=0,mask4=0):
-		super(ATT_J,self).__init__(train='train')
-		# self.train=train
-		self.mask1=mask1
-		self.mask2=mask2
-		self.mask3=mask3
-		self.mask4=mask4
-
-		self.stop1=stop1
-		self.stop2=stop2
-		self.stop3=stop3
-		self.stop4=stop4
-		# self.mask3,self.stop3=mask3,stop3
-		self.build()
+class Joint(LSTMCRF):
+	def __init__(self, args):
+		super(Joint,self).__init__(args)
+		if args.algorithm=='Joint':
+			self.build()
 
 	def build(self):
 		self.mix()
-		if self.train=='train':
+		if self.args.mode in ['train','tune']:
 			self.loss_layer()
 
 	def mix(self):
-		# self.word_embed=self.word_embedding_layer()
-		# self.char_embed=self.char_embedding_layer_lstm()
-		# assert(self.stop1==1)
-		
-		# if self.mask1==1 or self.stop1==1:
-		# 	self.base_embed=tf.stop_gradient(self.base_embed)
-			
 		encode1=self.mix_lstm1('net1')
 		encode2=self.mix_lstm1('net2')
-		# encode3=self.mix_lstm1('net3')
-		# encode4=self.mix_lstm1('net4')
-		
-		# encode1=self.mix_stacka('net1')
-		# encode2=self.mix_stacka('net2')
-
-		# encode1=self.mix_stackb()
-		# encode2=self.mix_stackb('cnn2')
-
-
-		# output1=self.dense(encode1,600,'dense11',activation=tf.nn.relu,bias=False)*self.mask_x
-		# output2=self.dense(encode2,600,'dense12',activation=tf.nn.relu,bias=False)*self.mask_x
-
-		# output1=tf.nn.dropout(output1,self.dropout_keep_prob)
-		# output2=tf.nn.dropout(output2,self.dropout_keep_prob)
 
 		# this place is easy to mistake
-		if self.mask1==1:
+		if self.args.mask_net1==1:
 			encode1=encode1*0.0
-		if self.stop1==1:
+		if self.args.gradient_stop_net1==1:
 			encode1=tf.stop_gradient(encode1)
 
-		if self.mask2==1:
+		if self.args.mask_net2==1:
 			encode2=encode2*0.0
-		if self.stop2==1:
+		if self.args.gradient_stop_net2==1:
 			encode2=tf.stop_gradient(encode2)
-
-		# if self.mask3==1:
-		# 	encode3=encode3*0.0
-		# # you error write self.stop1==1 here
-		# if self.stop3==1:
-		# 	encode3=tf.stop_gradient(encode3)
-
-		# if self.mask4==1:
-		# 	encode4=encode4*0.0
-		# if self.stop4==1:
-		# 	encode4=tf.stop_gradient(encode4)
 		
-
 		output=[]
-		if self.stop1 * self.stop2:
+		if self.args.gradient_stop_net1 * self.args.gradient_stop_net2:
 			tf.summary.image('encode1',tf.expand_dims(tf.expand_dims(encode1,axis=-1)[0],dim=0))
 			tf.summary.image('encode2',tf.expand_dims(tf.expand_dims(encode2,axis=-1)[0],dim=0))
 			# tf.summary.image('encode3',tf.expand_dims(tf.expand_dims(encode3,axis=-1)[0],dim=0))
 			# tf.summary.image('encode4',tf.expand_dims(tf.expand_dims(encode4,axis=-1)[0],dim=0))
 			encode=tf.concat([encode1,encode2],axis=-1)
-			print 'hahahahahhha'
 			
 			encode=self.dense(encode,100,'dense3-1',linear=False,bias=True)*self.mask_x
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)
 			output=self.dense(encode,self.output_class_num,'dense3-2',linear=True,bias=True)*self.mask_x
 		
-		# if self.stop1 * self.stop2 * self.stop3 * self.stop4:
-		# 	# encode=tf.concat([encode1,encode2,encode3,encode4],axis=-1)
-		# 	print 'hahahahahhha'
-		# 	output1=self.dense(encode1,self.output_class_num,'l-english2',linear=False,bias=True)*self.mask_x
-		# 	output2=self.dense(encode2,self.output_class_num,'l-german2',linear=False,bias=True)*self.mask_x
-		# 	output3=self.dense(encode3,self.output_class_num,'l-spanish2',linear=False,bias=True)*self.mask_x
-		# 	output4=self.dense(encode4,self.output_class_num,'l-dutch2',linear=False,bias=True)*self.mask_x
-		# 	# encode=tf.concat([output1,output2,output3,output4],axis=1)
-		# 	ave=tf.reduce_mean([output2,output3,output4],axis=0)
-		# 	x_by_y,_=self.attend(
-		# 	output1
-		# 	,ave
-		# 	,self.mask_x,self.mask_x
-		# 	,tp='ny',name='eng-all'
-		# 	# ,mask_self=True
-		# 	# ,dim=296
-		# 	)
-		# 	agg=tf.concat([output1,x_by_y],axis=-1)
-		# 	# encode=self.dense(encode,100,'dense3-1',linear=False,bias=True)*self.mask_x
-		# 	# encode=tf.nn.dropout(encode,self.dropout_keep_prob)
-		# 	output=self.dense(agg,self.output_class_num,'dense3-2',linear=True,bias=True)*self.mask_x
 		else:
 			# output1 : 50
 			output1=self.dense(encode1,self.output_class_num,'l-left',linear=True,bias=True)*self.mask_x
@@ -116,10 +53,13 @@ class ATT_J(LSTMCRF):
 			# output3=self.dense(encode3,self.output_class_num,'l-spanish',linear=True,bias=True)*self.mask_x
 			# output4=self.dense(encode4,self.output_class_num,'l-dutch',linear=True,bias=True)*self.mask_x
 			
-			if self.mask1==0:
+			if self.args.mask_net1==0:
 				output=output1
-			if self.mask2==0:
+			if self.args.mask_net2==0:
 				output=output2
+
+			# output=output1
+
 			# elif self.mask3==0:
 			# 	output=output3
 			# elif self.mask4==0:
@@ -128,13 +68,16 @@ class ATT_J(LSTMCRF):
 		self.output=output
 		with tf.variable_scope('crf'):
 			dim_crf1=self.output_class_num
-			crf_log_likelihood_lstm1,transition_params_lstm1,W_crf_lstm1,b_crf_lstm1=self.crf_loss_builtin(
+			crf_log_likelihood_lstm1,transition_params_lstm1,W_crf_lstm1,b_crf_lstm1= \
+			self.crf_loss_builtin(
 				output
 				,self.y_tag_sparse
-				,dim_crf1)
-			print 'did not use len in crf'
+				,dim_crf1
+				,test=False
+				,max_length=self.max_length
+				,batch_actual_length=self.batch_actual_length)
+
 			loss=tf.reduce_mean(-1.0*crf_log_likelihood_lstm1)
-			# loss=tf.reduce_sum(-1.0*tf.squeeze(self.batch_att,-1)*crf_log_likelihood_lstm1)
 			tf.summary.scalar('crf_loss',loss)
 			self.loss=loss
 
@@ -143,233 +86,15 @@ class ATT_J(LSTMCRF):
 				,b_crf_lstm1
 				,transition_params_lstm1
 				,dim_crf1)
+		print('class-num...',self.output_class_num)
 
 
-	# def mix(self):
-	# 	# self.word_embed=self.word_embedding_layer()
-	# 	# self.char_embed=self.char_embedding_layer_lstm()
-
-	# 	self.base_embed=self.word_embedding_layer_base()
-	# 	encode1=self.mix_lstm1('net1')
-	# 	encode2=self.mix_lstm1('net2')
-		
-	# 	# encode1=self.mix_stacka('net1')
-	# 	# encode2=self.mix_stacka('net2')
-
-	# 	# encode1=self.mix_stackb()
-	# 	# encode2=self.mix_stackb('cnn2')
-
-
-	# 	# output1=self.dense(encode1,600,'dense11',activation=tf.nn.relu,bias=False)*self.mask_x
-	# 	# output2=self.dense(encode2,600,'dense12',activation=tf.nn.relu,bias=False)*self.mask_x
-
-	# 	# output1=tf.nn.dropout(output1,self.dropout_keep_prob)
-	# 	# output2=tf.nn.dropout(output2,self.dropout_keep_prob)
-
-	# 	if self.mask1==1:
-	# 		encode1=encode1*0.0
-	# 	if self.stop1==1:
-	# 		encode1=tf.stop_gradient(encode1)
-
-	# 	if self.mask2==1:
-	# 		encode2=encode2*0.0
-	# 	if self.stop2==1:
-	# 		encode2=tf.stop_gradient(encode2)
-
-	# 	# if self.mask3:
-	# 	# 	encode3=encode3*0.0
-	# 	# if self.stop3:
-	# 	# 	encode3=tf.stop_gradient(encode3)
-		
-
-	# 	output=[]
-	# 	if self.stop1 * self.stop2:
-	# 		print 'hahahahahhha'
-	# 		encode=tf.concat([encode1,encode2],axis=-1)
-			
-	# 		encode=self.dense(encode,600,'dense3-1',linear=False,bias=True)*self.mask_x
-	# 		encode=tf.nn.dropout(encode,self.dropout_keep_prob)
-	# 		output=self.dense(encode,self.output_class_num,'dense3-2',linear=True,bias=True)*self.mask_x
-	# 	else:
-	# 		# output1 : 50
-	# 		output1=self.dense(encode1,self.output_class_num,'l-1',linear=True,bias=True)*self.mask_x
-	# 		output2=self.dense(encode2,self.output_class_num,'l-2',linear=True,bias=True)*self.mask_x
-	# 		if self.mask1==0:
-	# 			output=output1
-	# 		if self.mask2==0:
-	# 			output=output2
-
-	# 	self.output=output
-	# 	with tf.variable_scope('crf'):
-	# 		dim_crf1=self.output_class_num
-	# 		crf_log_likelihood_lstm1,transition_params_lstm1,W_crf_lstm1,b_crf_lstm1=self.crf_loss_builtin(
-	# 			output
-	# 			,self.y_tag_sparse
-	# 			,dim_crf1)
-	# 		print 'did not use len in crf'
-	# 		loss=tf.reduce_mean(-1.0*crf_log_likelihood_lstm1)
-	# 		# loss=tf.reduce_sum(-1.0*tf.squeeze(self.batch_att,-1)*crf_log_likelihood_lstm1)
-	# 		tf.summary.scalar('crf_loss',loss)
-	# 		self.loss=loss
-
-	# 		self.viterbi_sequence=self.crf_predict(output
-	# 			,W_crf_lstm1
-	# 			,b_crf_lstm1
-	# 			,transition_params_lstm1
-	# 			,dim_crf1)
-
-
-	# def mix(self):
-	"""
-	In NER paper
-	"""
-	# 	# self.word_embed=self.word_embedding_layer()
-	# 	# self.char_embed=self.char_embedding_layer_lstm()
-	# 	self.base_embed=self.word_embedding_layer_base()
-	# 	# encode1=self.mix_lstm1()
-	# 	# encode2=self.mix_lstm1('baseline2')
-		
-	# 	encode1=self.mix_stacka()
-	# 	encode2=self.mix_stacka('cnn2')
-
-	# 	# encode1=self.mix_stackb()
-	# 	# encode2=self.mix_stackb('cnn2')
-
-
-	# 	# output1=self.dense(encode1,600,'dense11',activation=tf.nn.relu,bias=False)*self.mask_x
-	# 	# output2=self.dense(encode2,600,'dense12',activation=tf.nn.relu,bias=False)*self.mask_x
-
-	# 	# output1=tf.nn.dropout(output1,self.dropout_keep_prob)
-	# 	# output2=tf.nn.dropout(output2,self.dropout_keep_prob)
-
-	# 	if self.mask1:
-	# 		encode1=encode1*0.0
-	# 	if self.stop1:
-	# 		encode1=tf.stop_gradient(encode1)
-
-	# 	if self.mask2:
-	# 		encode2=encode2*0.0
-	# 	if self.stop2:
-	# 		encode2=tf.stop_gradient(encode2)
-
-	# 	# if self.mask3:
-	# 	# 	encode3=encode3*0.0
-	# 	# if self.stop3:
-	# 	# 	encode3=tf.stop_gradient(encode3)
-		
-	# 	encode=tf.concat([encode1,encode2],axis=-1)
-
-	# 	# =====
-	# 	if self.stop1 * self.stop2 or self.mask1+self.mask2==0:
-	# 		print 'hahahahahhha'
-	# 		# output1=self.dense(encode1,50,'dense21',linear=True,bias=False)*self.mask_x
-	# 		# output2=self.dense(encode2,50,'dense22',linear=True,bias=False)*self.mask_x
-	# 		# encode=tf.concat([output1,output2],axis=-1)
-	# 		# # ===this is the original====
-	# 		# output=self.dense(encode,100,'dense3',linear=True,bias=False)*self.mask_x
-
-	# 		# output=self.dense(encode,100,'dense3',linear=True,bias=False)*self.mask_x
-			
-	# 		encode=self.dense(encode,600,'dense3-1',linear=False,bias=True)*self.mask_x
-	# 		encode=tf.nn.dropout(encode,self.dropout_keep_prob)
-	# 		output=self.dense(encode,100,'dense3-2',linear=True,bias=False)*self.mask_x
-	# 	else:
-	# 		output1=self.dense(encode1,50,'o1',linear=True,bias=False)*self.mask_x
-	# 		output2=self.dense(encode2,50,'oo',linear=True,bias=False)*self.mask_x
-	# 		output=tf.concat([output1,output2],axis=-1)
-	# 		output=self.dense(output,100,'dense2',linear=True,bias=False)*self.mask_x
-
-	# 	self.output=output
-	# 	with tf.variable_scope('crf'):
-	# 		dim_crf1=100
-	# 		crf_log_likelihood_lstm1,transition_params_lstm1,W_crf_lstm1,b_crf_lstm1=self.crf_loss_builtin(
-	# 			output
-	# 			,self.y_tag_sparse
-	# 			,dim_crf1)
-	# 		print 'did not use len in crf'
-	# 		loss=tf.reduce_mean(-1.0*crf_log_likelihood_lstm1)
-	# 		# loss=tf.reduce_sum(-1.0*tf.squeeze(self.batch_att,-1)*crf_log_likelihood_lstm1)
-	# 		tf.summary.scalar('crf_loss',loss)
-	# 		self.loss=loss
-
-	# 		self.viterbi_sequence=self.crf_predict(output
-	# 			,W_crf_lstm1
-	# 			,b_crf_lstm1
-	# 			,transition_params_lstm1
-	# 			,dim_crf1)
-
-
-	# def mix(self):
-	# 	self.base_embed=self.word_embedding_layer_base()
-	# 	encode1=self.mix_lstm1()
-
-	# 	if self.mask1:
-	# 		encode1=encode1*0.0
-	# 	if self.stop1:
-	# 		encode1=tf.stop_gradient(encode1)
-
-	# 	self.lm=encode1
-
-	# 	encode2=self.mix_lstm4()
-
-	# 	if self.mask2:
-	# 		encode2=encode2*0.0
-	# 	if self.stop2:
-	# 		encode2=tf.stop_gradient(encode2)
-		
-	# 	if self.stop3==0:
-	# 		# =====
-	# 		if self.stop1 * self.stop2 or self.mask1+self.mask2==0:
-	# 			print 'hahahahahhha'
-	# 			output=self.dense(encode2,100,'dense3',linear=True,bias=False)*self.mask_x
-	# 		else:
-	# 			output=self.dense(encode2,100,'o2',linear=True,bias=False)*self.mask_x
-			
-	# 		self.output=output
-			
-	# 		with tf.variable_scope('crf'):
-	# 			dim_crf1=100
-	# 			crf_log_likelihood_lstm1,transition_params_lstm1,W_crf_lstm1,b_crf_lstm1=self.crf_loss_builtin(
-	# 				output
-	# 				,self.y_tag_sparse
-	# 				,dim_crf1)
-	# 			print 'did not use len in crf'
-	# 			loss=tf.reduce_mean(-1.0*crf_log_likelihood_lstm1)
-	# 			# loss=tf.reduce_sum(-1.0*tf.squeeze(self.batch_att,-1)*crf_log_likelihood_lstm1)
-	# 			tf.summary.scalar('crf_loss',loss)
-	# 			self.loss=loss
-
-	# 			self.viterbi_sequence=self.crf_predict(output
-	# 				,W_crf_lstm1
-	# 				,b_crf_lstm1
-	# 				,transition_params_lstm1
-	# 				,dim_crf1)
-	# 	else:
-	# 		s=tf.shape(encode1)
-	# 		output1=tf.layers.dense(encode1,self.build_vocab_size,use_bias=False)
-
-	# 		output1=output1[:,:-1]
-	# 		output1=tf.reshape(output1,[-1,self.build_vocab_size])
-	# 		self.output=output1
-
-	# 		target=self.x_data_random[:,1:]
-	# 		target=tf.reshape(target,[-1])
-
-	# 		loss=tf.contrib.legacy_seq2seq.sequence_loss_by_example([output1], [target], [tf.ones(s[0]*(s[1]-1))])
-	# 		loss=tf.reduce_sum(loss)/tf.cast(s[0],tf.float32)
-	# 		tf.summary.scalar('lm_loss',loss)
-	# 		self.loss=loss
-
+	# These sample blocks will be further organized, and this toolkit will provide more easy-to-use blocks.
 	def mix_lstm1_oov(self,name='baseline1'):
 		with tf.variable_scope(name):
-			# word_embed=self.base_embed
 			word_embed=self.word_embedding_layer_share(self.base_embed)
 			char_embed=self.char_embedding_layer_lstm()
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-			# concat_embed=word_embed
 			concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -377,29 +102,17 @@ class ATT_J(LSTMCRF):
 
 	def mix_lstm1(self,name='baseline1'):
 		with tf.variable_scope(name):
-			concat_embed=self.word_embedding_layer_base()
-			# word_embed=self.word_embedding_layer_share(self.base_embed)
-			# char_embed=self.char_embedding_layer_lstm()
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-			# concat_embed=word_embed
-			# concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
+			word_embed=self.word_embedding_layer_base()
+			char_embed=self.char_embedding_layer_lstm()
+			concat_embed=tf.concat([word_embed,char_embed],axis=-1)*self.mask_x
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
-			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
+			encode=tf.nn.dropout(encode*self.mask_x,self.dropout_keep_prob)
 			return encode
 
 	def mix_lstm_random(self,name='baseline1'):
 		with tf.variable_scope(name):
 			concat_embed=self.word_embedding_layer_random()
-			# word_embed=self.word_embedding_layer_share(self.base_embed)
-			# char_embed=self.char_embedding_layer_lstm()
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-			# concat_embed=word_embed
-			# concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -410,11 +123,7 @@ class ATT_J(LSTMCRF):
 		with tf.variable_scope(name):
 			word_embed=self.word_embedding_layer_share(self.base_embed)
 			char_embed=self.char_embedding_layer_lstm()
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-
 			concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -426,11 +135,7 @@ class ATT_J(LSTMCRF):
 		with tf.variable_scope(name):
 			word_embed=self.word_embedding_layer_share(self.base_embed)
 			char_embed=self.char_embedding_layer_cnn()
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-
 			concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode=self.cnn_layer(1,300,concat_embed)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -443,11 +148,7 @@ class ATT_J(LSTMCRF):
 			base_embed=self.word_embedding_layer_google()
 			word_embed=self.word_embedding_layer_share100(base_embed)
 			char_embed=self.char_embedding_layer_lstm('char_embedding_2')
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-
 			concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -457,11 +158,7 @@ class ATT_J(LSTMCRF):
 		with tf.variable_scope('basline_net3'):
 			word_embed=self.word_embedding_layer_share(self.base_embed)
 			char_embed=self.char_embedding_layer_lstm('char_embedding_2')
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-
 			concat_embed=tf.concat([word_embed,char_embed],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -471,11 +168,7 @@ class ATT_J(LSTMCRF):
 		with tf.variable_scope('basline_net4'):
 			word_embed=self.word_embedding_layer_share(self.base_embed)
 			char_embed=self.char_embedding_layer_lstm('char_embedding_2')
-			# word_embed=self.word_embed
-			# char_embed=self.char_embed
-
 			concat_embed=tf.concat([word_embed,char_embed,self.lm],axis=-1)
-			# self.concat_embed=concat_embed
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
 			encode,cell,_,_=self.lstm(concat_embed,hidden=self.hidden_dim)
 			encode=tf.nn.dropout(encode,self.dropout_keep_prob)*self.mask_x
@@ -585,20 +278,6 @@ class ATT_J(LSTMCRF):
 				# ,mask_self=True
 				,dim=100
 				)
-
-
-			# mean=tf.reduce_sum(word_embed,axis=1)/x_len
-			# mean=tf.expand_dims(mean,axis=1)
-			# s=tf.shape(word_embed)
-			# mean_att,_=self.attend(
-			# 	word_embed
-			# 	,mean
-			# 	,self.mask_x,tf.ones([s[0],1,1],dtype=tf.float32)
-			# 	,tp='ny',name='attendmean'
-			# 	# ,mask_self=True
-			# 	,dim=200
-			# 	)
-			# self.mean_att=mean_att
 
 			concat_embed=tf.concat([x_by_y,x_by_yc],axis=-1)
 			concat_embed=tf.nn.dropout(concat_embed,self.dropout_keep_prob)
